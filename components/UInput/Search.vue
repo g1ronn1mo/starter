5cc76a2c63c692527d00f9fbca7ae3d8409a8df9
Benedikt Schwab <beneschwab92@gmail.com>
2024-03-01 11:00:30 +0000

    changed some things

---
+++ components/UInput/Search.vue
@@ -0,0 +1,26 @@
+  
+<script setup lang="ts">
+    const q = ref('')
+</script>
+
+<template>
+    <UInput
+      v-model="q"
+      name="q"
+      placeholder="Search..."
+      icon="i-heroicons-magnifying-glass-20-solid"
+      autocomplete="off"
+      :ui="{ icon: { trailing: { pointer: '' } } }"
+    >
+      <template #trailing>
+        <UButton
+          v-show="q !== ''"
+          color="gray"
+          variant="link"
+          icon="i-heroicons-x-mark-20-solid"
+          :padded="false"
+          @click="q = ''"
+        />
+      </template>
+    </UInput>
+  </template>
